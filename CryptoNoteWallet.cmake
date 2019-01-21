0fd0726967b8c7bc10f253cf88e413ce4c7c2b90
thinkblockchain <46258503+thinkblockchain@users.noreply.github.com>
2019-01-21 18:54:05 +0500

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "commoditycoin")
+set(CN_CURRENCY_DISPLAY_NAME "Commodity Coin")
+set(CN_CURRENCY_TICKER "comc")
